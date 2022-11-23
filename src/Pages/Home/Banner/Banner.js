6f449bb1953995e391ea193f4f2f94be6c1156dc
mamun-prodhan <mamunpr9@gmail.com>
2022-11-23 19:18:36 +0600

    banner slider added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,52 +3,58 @@ import img1 from '../../../assets/slider/slider1.jpg';
 import img2 from '../../../assets/slider/slider2.jpg';
 import img3 from '../../../assets/slider/slider3.jpg';
 import img4 from '../../../assets/slider/slider4.jpg';
+import './Banner.css'
+import BannerItem from './BannerItem';
+
+
+const bannerData = [
+    {
+        image: img1,
+        prev: 4,
+        id: 1,
+        next: 2
+    },
+    {
+        image: img2,
+        prev: 1,
+        id: 2,
+        next: 3
+    },
+    {
+        image: img3,
+        prev: 2,
+        id: 3,
+        next: 4
+    },
+    {
+        image: img4,
+        prev: 3,
+        id: 4,
+        next: 1
+    }
+]
 
 const Banner = () => {
     return (
         <div className="carousel w-full">
-            <div id="slide1" className="carousel-item relative w-full">
-                <img src={img1} alt="" className="w-full" />
-                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
-                    <h1 className='text-6xl font-bold text-white'>
-                        Let's Plan the <br />
-                        Trip of Your <br />
-                        Dreams
-                    </h1>
-                </div>
-                <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
-                    <p className='text-xl text-white'>IMMIGO Consultants is one of the best and leading Student Visa Consultancy Firm in Bangladesh offers comprehensive educational services to students who have strong desire and intention of studying in internationally well reputed institutions in the UK, Canada, USA, Australia, and Malaysia.</p>
-                </div>
-                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
-                    <button className="btn btn-warning mr-5">Warning</button>
-                    <button className="btn btn-outline btn-warning">Warning</button>
-                </div>
-                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
-                    <a href="#slide2" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide2" className="carousel-item relative w-full">
+
+            {
+                bannerData.map(slide => <BannerItem
+                key={slide.id}
+                slide={slide}
+                ></BannerItem>)
+            }
+
+
+            {/* <BannerItem image={img1}></BannerItem> */}
+            {/* <div id="slide2" className="carousel-item relative w-full">
                 <img src={img2} alt="" className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
-            </div>
-            <div id="slide3" className="carousel-item relative w-full">
-                <img src={img3} alt="" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a>
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-                <img src={img4} alt="" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
-                    <a href="#slide1" className="btn btn-circle">❯</a>
-                </div>
-            </div>
+            </div> */}
+            
         </div>
     );
 };
---
+++ src/Pages/Home/Banner/BannerItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const BannerItem = ({slide}) => {
+    const {image, id, prev, next} = slide;
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+                <div className='carousel-img'>
+                    <img src={image} alt="" className="w-full" />
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/4">
+                    <h1 className='text-6xl font-bold text-white'>
+                        Let's Plan the <br />
+                        Trip of Your <br />
+                        Dreams
+                    </h1>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 w-2/5 left-24 top-1/2">
+                    <p className='text-xl text-white'>IMMIGO Consultants is one of the best and leading Student Visa Consultancy Firm in Bangladesh offers comprehensive educational services to students who have strong desire and intention of studying in internationally well reputed institutions in the UK, Canada, USA, Australia, and Malaysia.</p>
+                </div>
+                <div className="absolute flex justify-start transform -translate-y-1/2 w-2/5 left-24 top-3/4">
+                    <button className="btn btn-warning mr-5">Warning</button>
+                    <button className="btn btn-outline btn-warning">Warning</button>
+                </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
+                    <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+                </div>
+            </div>
+    );
+};
+
+export default BannerItem;

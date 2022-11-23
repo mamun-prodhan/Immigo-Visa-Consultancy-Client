150a832b4b6f28f56e09aa397379808176e0e15c
mamun-prodhan <mamunpr9@gmail.com>
2022-11-23 20:11:47 +0600

    slider bug fixed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,7 +3,6 @@ import img1 from '../../../assets/slider/slider1.jpg';
 import img2 from '../../../assets/slider/slider2.jpg';
 import img3 from '../../../assets/slider/slider3.jpg';
 import img4 from '../../../assets/slider/slider4.jpg';
-import './Banner.css'
 import BannerItem from './BannerItem';
 
 
@@ -36,7 +35,7 @@ const bannerData = [
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full py-10">
 
             {
                 bannerData.map(slide => <BannerItem
@@ -44,17 +43,6 @@ const Banner = () => {
                 slide={slide}
                 ></BannerItem>)
             }
-
-
-            {/* <BannerItem image={img1}></BannerItem> */}
-            {/* <div id="slide2" className="carousel-item relative w-full">
-                <img src={img2} alt="" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
-                    <a href="#slide3" className="btn btn-circle">❯</a>
-                </div>
-            </div> */}
-            
         </div>
     );
 };

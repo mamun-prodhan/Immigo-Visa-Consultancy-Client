10d70a5044896c9879e27456f993ee3ede6c9290
mamun-prodhan <mamunpr9@gmail.com>
2022-11-26 14:19:39 +0600

    react photo view added

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
-const ServiceCard = ({service}) => {
-    const {_id, img, price, title, description} = service;
+const ServiceCard = ({ service }) => {
+    const { _id, img, price, title, description } = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="serviceImage" /></figure>
+
+            <PhotoProvider>
+                <PhotoView src={img}>
+                    <figure><img src={img} alt="serviceImage" /></figure>
+                </PhotoView>
+            </PhotoProvider>
+
+
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p className='text-2xl text-orange-600 font-bold'>Price: ${price}</p>
                 <p className='text-justify'>
                     {description.length > 100 ?
-                    <>{description.slice(0, 100) + ' ...'}</>
-                    :
-                    description
-                }
+                        <>{description.slice(0, 100) + ' ...'}</>
+                        :
+                        description
+                    }
                 </p>
                 <div className="card-actions justify-start">
                     <Link to={`/details/${_id}`}>
--- src/Pages/ServicesPage/ServicePageCard.js
+++ src/Pages/ServicesPage/ServicePageCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const ServicePageCard = ({ service }) => {
     const { _id, img, price, title, description } = service;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="serviceImage" /></figure>
+            <PhotoProvider>
+                <PhotoView src={img}>
+                    <figure><img src={img} alt="serviceImage" /></figure>
+                </PhotoView>
+            </PhotoProvider>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p className='text-2xl text-orange-600 font-bold'>Price: ${price}</p>

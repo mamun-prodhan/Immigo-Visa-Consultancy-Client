c642e8d40f3f17a43fe2275e52fa731622359414
mamun-prodhan <mamunpr9@gmail.com>
2022-12-21 22:18:00 +0600

    responsive about me completed

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -9,13 +9,13 @@ const About = () => {
     return (
         <div className="hero bg-base-200 my-20">
             <div className="hero-content flex-col lg:flex-row-reverse p-6 lg:p-14">
-                <div className='w-1/2  relative'>
+                <div className='w-full md:w-1/2  relative'>
                     <img src={image3} alt="" className="rounded-lg shadow-2xl" />
                     <img src={image4} alt="" className="absolute left-16 top-3/4 rounded-lg shadow-2xl" />
                     
                 </div>
-                <div className='w-1/2'>
-                    <p className='text-orange-600 font-bold text-2xl'>About Me</p>
+                <div className='w-full md:w-1/2'>
+                    <p className='text-orange-600 font-bold text-2xl mt-8 md:mt-0'>About Me</p>
                     <h1 className="text-5xl font-bold  my-5">Experienced Visa <br />Consultant</h1>
                     <p className="">IMMIGO Visa Consultancy is one of the leading companies supporting applicants for Immigration, Tourist Visa, Medical Tourism and expanding into student visa support. With locations in Bangladesh, Thailand, Singapore, UK and Australia, we are well equipped with lawyers who would help you in pursuit of your immigration.</p>
 
